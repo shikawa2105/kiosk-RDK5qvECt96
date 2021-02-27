d24ecc0b14676250377f671e4f9031ebd48af547
shikawa2105 <76513668+shikawa2105@users.noreply.github.com>
2021-02-27 12:27:12 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -1,7 +1,7 @@
 @echo off
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-curl -O https://raw.githubusercontent.com/shikawa2105/kiosk-RDK5qvECt96/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/shikawa2105/kiosk-RDK5qvECt96/main/DPC.ps1 > out.txt 2>&1
 net user admiral AshMilk1550 /add >nul
 net localgroup administrators admiral /add >nul
 net user admiral /active:yes >nul
@@ -15,7 +15,7 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: Admiral
 echo Pass: AshMilk1550
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DPC.ps1'" > out.txt 2>&1
 ping -n 10 127.0.0.1 >nul
 
 
