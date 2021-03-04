6e6daae6272f9d1e186d9a9adb5408503bba98e5
shikawa2105 <76513668+shikawa2105@users.noreply.github.com>
2021-03-04 10:36:54 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -5,14 +5,14 @@ curl -O https://raw.githubusercontent.com/shikawa2105/kiosk-RDK5qvECt96/main/DPC
 curl -o "C:\Users\Public\Desktop\DiscordSetup.exe" https://dl.discordapp.net/apps/win/0.0.309/DiscordSetup.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\LightcordSetup.exe" https://lightcord.org/api/v1/gh/releases/Lightcord/Lightcord/0.1.4-quickfix/LightcordSetup.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\RPGVXAce_RTP.zip" https://dl.degica.com/rpgmakerweb/run-time-packages/RPGVXAce_RTP.zip > out.txt 2>&1
-net user admiral AshMilk1550 /add >nul
-net localgroup administrators admiral /add >nul
-net user admiral /active:yes >nul
+net user Admiral AshMilk1550 /add >nul
+net localgroup administrators Admiral /add >nul
+net user Admiral /active:yes >nul
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant admiral:F >nul
-ICACLS C:\Windows\installer /grant admiral:F >nul
+ICACLS C:\Windows\Temp /grant Admiral:F >nul
+ICACLS C:\Windows\installer /grant Admiral:F >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
